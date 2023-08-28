e18134928d206a10a570edccc9f5161fee5c96d1
MohammedLaham <prog.khan.2002@gmail.com>
2023-08-28 20:47:03 +0300

    add weather full

---
+++ lib/cubits/weather_cubit/weather_state.dart
@@ -0,0 +1,15 @@
+import 'package:weather_app/models/weather_model.dart';
+
+abstract class WeatherState{
+
+}
+class WeatherInitial extends WeatherState{}
+class WeatherLoading extends WeatherState{
+
+}
+class WeatherSuccess extends WeatherState{
+  WeatherModel weatherModel;
+
+  WeatherSuccess(this.weatherModel);
+}
+class WeatherFailure extends WeatherState{}
